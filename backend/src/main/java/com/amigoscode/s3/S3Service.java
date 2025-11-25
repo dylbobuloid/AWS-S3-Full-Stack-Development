cbb46b6cb53d71873b5f8fa6be490a2cc93488d6
dylbobuloid <majestickookie@gmail.com>
2025-11-25 19:25:58 +0000

    Implemented downloading and uploading a file to bucket

--- backend/src/main/java/com/amigoscode/s3/S3Service.java
+++ backend/src/main/java/com/amigoscode/s3/S3Service.java
@@ -1,6 +1,5 @@
 package com.amigoscode.s3;
 
-import org.springframework.stereotype.Service;
 import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -10,31 +9,33 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
 
-@Service
 public class S3Service {
 
-    private final S3Client s3;
+    private final S3Client s3Client;
 
-    public S3Service(S3Client s3) {
-        this.s3 = s3;
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
     }
 
+
+    // UPLOADING THE OBJECT ONTO S3 BUCKET
+    // This method allows us to store files toa specific bucket using a key
+    // and using the file as the byte array
     public void putObject(String bucketName, String key, byte[] file) {
         PutObjectRequest objectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
                 .key(key)
                 .build();
-        s3.putObject(objectRequest, RequestBody.fromBytes(file));
+        s3Client.putObject(objectRequest, RequestBody.fromBytes(file));
     }
 
     public byte[] getObject(String bucketName, String key) {
-        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+        GetObjectRequest objectRequest = GetObjectRequest.builder()
                 .bucket(bucketName)
                 .key(key)
                 .build();
 
-        ResponseInputStream<GetObjectResponse> res = s3.getObject(getObjectRequest);
-
+        ResponseInputStream<GetObjectResponse> res = s3Client.getObject(objectRequest);
         try {
             return res.readAllBytes();
         } catch (IOException e) {
